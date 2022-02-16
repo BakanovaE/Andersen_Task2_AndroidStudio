c46fef7b590da1d2f1c6fd1498ab1cad74054af9
Ekaterina Bakanova <k125598@yandex.ru>
2022-02-16 15:12:57 +0300

    Initial commit

---
+++ app/src/main/java/org/martellina/androidstudio/MainActivity.kt
@@ -0,0 +1,29 @@
+package org.martellina.androidstudio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.task1_2 -> {
+                val intent = Intent(this, HelloToastActivity::class.java)
+                startActivity(intent)
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
